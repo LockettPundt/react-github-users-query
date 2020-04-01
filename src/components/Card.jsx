a70760d648de041fc1409aa4b1f9deb4a85cdb6a
Lockett Pundt <kalifornium@hotmail.com>
2020-04-01 16:34:58 -0400

    created a link for each user card that will direct them to the individual user route when clicked.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 
 const Card = props => {
-  const { blog, name, avatar_url, location} = props;
+  console.log(props);
+  const { blog, name, avatar_url, location, login} = props;
+  const profileLink = `/user/${login}`;
   
   return (
   <div>
     <div >
       <img alt="avatar"src={avatar_url} />
       <div className="userInfo">
-        <p>{name}</p>
+        <a href={profileLink}>{name}</a>
         <p><a href={blog}>{blog}</a></p>
         <p>{location}</p>
       </div>
-  </div>
+    </div>
   </div>
   );
 };
